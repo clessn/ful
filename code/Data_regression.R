5c7e37bcbc1b203c69dbfe349e61287b5785a590
AlexBouillon <alexandrebouillon7@gmail.com>
2022-12-04 17:25:49 -0500

    régressions FUL

---
+++ code/Data_regression.R
@@ -0,0 +1,112 @@
+library(tidyverse)
+
+
+Data <- readRDS("_SharedFolder_fondation-ulaval/Data/CleanData.rds") %>% 
+  mutate(donated = ifelse(historic_lastDonation_amount > 0, 1, 0))
+
+table(Data$donated)
+names(Data)
+table(Data$historic_donationLevel)
+table(Data$historic_donationLevel_gouverneur)
+
+Data$historic_donationLevelVD <- 0
+Data$historic_donationLevelVD[Data$historic_donationLevel == "0.125"] <- 0.33
+Data$historic_donationLevelVD[Data$historic_donationLevel == "0.25"] <- 0.66
+Data$historic_donationLevelVD[Data$historic_donationLevel >= "0.375"] <- 1
+table(Data$historic_donationLevelVD)
+
+
+table(Data$emploi_teacherUlaval)
+
+options(digits=4)
+
+table(Data$contact_declineCommunications_number)
+hist(Data$contact_declineCommunications_number)
+Data$refusContact <- 0
+Data$refusContact <- log(Data$contact_declineCommunications_number)
+table(Data$refusContact)
+
+minmaxNormalization <- function(x) {
+  return((x-min(x, na.rm = T))/(max(x, na.rm = T)-min(x, na.rm = T)))
+}
+
+Data$refusContact <- 0
+Data$refusContact[Data$contact_declineCommunications_number == 1] <- 1
+Data$refusContact[Data$contact_declineCommunications_number == 2] <- 2
+Data$refusContact[Data$contact_declineCommunications_number == 3] <- 3
+Data$refusContact[Data$contact_declineCommunications_number == 4] <- 4
+Data$refusContact[Data$contact_declineCommunications_number == 5] <- 5
+Data$refusContact[Data$contact_declineCommunications_number == 6] <- 6
+Data$refusContact[Data$contact_declineCommunications_number == 7] <- 7
+Data$refusContact[Data$contact_declineCommunications_number == 8] <- 8
+Data$refusContact[Data$contact_declineCommunications_number == 9] <- 9
+Data$refusContact[Data$contact_declineCommunications_number == 10] <- 10
+Data$refusContact[Data$contact_declineCommunications_number == 11] <- 11
+Data$refusContact[Data$contact_declineCommunications_number == 12] <- 12
+Data$refusContact[Data$contact_declineCommunications_number >= 13] <- 13
+table(Data$refusContact)
+
+Data$normRefusContact <- 0
+Data$normRefusContact <- minmaxNormalization(Data$refusContact)
+table(Data$normRefusContact)
+
+table(Data$emploi_technicianUlaval)
+
+#Teacher
+DataTeacher <- Data %>% 
+  filter(emploi_teacherUlaval == 1)
+
+model1 <- glm(donated ~ ses_female + normRefusContact, data = DataTeacher, family = binomial())
+summary(model1)
+
+model2 <- glm(donated ~ ses_female + emploi_teacherUlaval + normRefusContact, data = Data, family = binomial())
+summary(model2)
+
+#Executive
+DataExecutive <- Data %>% 
+  filter(emploi_executiveUlaval == 1)
+
+model3 <-glm(donated ~ ses_female + emploi_executiveUlaval + normRefusContact, data = Data, family = binomial())
+summary(model3)
+
+model4 <- glm(donated ~ ses_female + normRefusContact, data = DataExecutive, family = binomial())
+summary(model4)
+
+#Retired
+DataRetired <- Data %>% 
+  filter(emploi_retiredUlaval == 1)
+
+model <-glm(donated ~ ses_female + emploi_retiredUlaval + normRefusContact, data = Data, family = binomial())
+summary(model)
+
+model <- glm(donated ~ ses_female + normRefusContact, data = DataRetired, family = binomial())
+summary(model)
+
+#Professional
+table(Data$emploi_professionalUlaval)
+
+DataProfessional <- Data %>% 
+  filter(emploi_professionalUlaval == 1)
+
+model <-glm(donated ~ ses_female + emploi_professionalUlaval + normRefusContact, data = Data, family = binomial())
+summary(model)
+
+model <- glm(donated ~ ses_female + normRefusContact, data = DataProfessional, family = binomial())
+summary(model)
+
+
+#Lecturer
+table(Data$emploi_lecturerUlaval)
+
+DataLecturer <- Data %>% 
+  filter(emploi_lecturerUlaval == 1)
+
+model <-glm(donated ~ ses_female + emploi_lecturerUlaval + normRefusContact, data = Data, family = binomial())
+summary(model)
+
+model <- glm(donated ~ ses_female + normRefusContact, data = DataLecturer, family = binomial())
+summary(model)
+
+
+
+
