8e6dff7cb02ca3dd599897249aea20157dd7fd78
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2022-12-04 16:53:46 -0500

    run jg graph

--- descriptive_hub.R
+++ descriptive_hub.R
@@ -14,11 +14,13 @@ ggplot(Data, aes(x = education_yearFirstGraduation)) +
                  color = "#E30513", alpha = 0.5) +
   scale_x_continuous(limits = c(1930, 2022)) +
   xlab("\nAnnée de promotion\ndu premier diplôme\n") +
-  ylab("Nombre d'étudiants\n") +
+  ylab("\nNombre d'étudiants\n") +
   clessnverse::theme_clean_light(base_size = 30) +
   theme(axis.ticks.x = element_blank(),
         axis.line.x = element_line(linewidth = 0.2),
-        text = element_text(lineheight = 0.3))
+        text = element_text(lineheight = 0.3),
+        axis.title.x = element_text(size = 45),
+        axis.title.y = element_text(size = 45))
 
 ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.2.distributionDiploma.png",
        width = 8, height = 6)
