d87072b2c9f717aef9ba11dd0ca719f34fd950da
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2022-12-05 18:01:39 -0500

    regressions

--- code/analyses_JG.R
+++ code/analyses_JG.R
@@ -35,14 +35,22 @@ ggplot(GraphData, aes(x = ses_female, y = prop)) +
   geom_bar(stat = "identity", aes(fill = ses_female, colour = ses_female, alpha = 40), show.legend = FALSE) +
   facet_wrap(~ donator) +
   scale_y_continuous(limits = c(0, 100)) +
-  geom_hline(yintercept = 50, linetype = "dashed") +
+  geom_hline(yintercept = 50, linetype = "dotted",
+             color = "#5B5B5B") +
   scale_x_discrete(breaks = c(0.0, 1.0), label = c("Hommes", "Femmes")) +
-  labs(y = "Proportion (%)\n", x = "") +
+  labs(y = "\nProportion (%)\n", x = "") +
   scale_fill_manual(values = c("#FFC103", "#E30513")) +
   scale_colour_manual(values = c("#FFC103", "#E30513")) +
   clessnverse::theme_clean_light(base_size = 30) +
-  theme(strip.background.x = element_rect(fill = "#D3D3D3", color = NA), text = element_text(lineheight = 0.3))
+  theme(strip.background.x = element_rect(fill = "#D3D3D3", color = NA),
+        text = element_text(lineheight = 0.3),
+        axis.ticks.x = element_blank(),
+        axis.line.x = element_blank(),
+        axis.title.y = element_text(size = 40),
+        axis.text.x = element_text(size = 30),
+        axis.text.y = element_text(size = 30))
 
 
-ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.1_gender.png") 
+ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.1_gender.png",
+       width = 7, height = 5) 
 
--- descriptive_hub.R
+++ descriptive_hub.R
@@ -12,19 +12,56 @@ Data <- readRDS("_SharedFolder_fondation-ulaval/Data/CleanData.rds")
 ggplot(Data, aes(x = education_yearFirstGraduation)) +
   geom_histogram(binwidth = 1, fill = "#E30513",
                  color = "#E30513", alpha = 0.5) +
-  scale_x_continuous(limits = c(1930, 2022)) +
-  xlab("\nAnnée de promotion\ndu premier diplôme\n") +
-  ylab("\nNombre d'étudiants\n") +
+  scale_x_continuous(limits = c(1930, 2022),
+                     expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0)) +
+  xlab("\nAnnée de promotion   \ndu premier diplôme   \n") +
+  ylab("\nNombre d'étudiants   \n") +
   clessnverse::theme_clean_light(base_size = 30) +
   theme(axis.ticks.x = element_blank(),
-        axis.line.x = element_line(linewidth = 0.2),
+        axis.line.x = element_line(linewidth = 0),
         text = element_text(lineheight = 0.3),
         axis.title.x = element_text(size = 45),
-        axis.title.y = element_text(size = 45))
+        axis.title.y = element_text(size = 45),
+        axis.text.x = element_text(size = 30),
+        axis.text.y = element_text(size = 30))
 
-ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.2.distributionDiploma.png",
-       width = 8, height = 6)
+ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.2.1.distributionDiploma.png",
+       width = 8, height = 5)
 
 # 1.1 first graduation ~ historic_dateFirstDonation
 
+class(Data$education_yearFirstGraduation)
+class(Data$historic_dateFirstDonation)
 
+Data$yearFirstDonation <- as.numeric(format(Data$historic_dateFirstDonation, "%Y"))
+class(Data$yearFirstDonation)
+
+Data$gap_Dipl_FirstDon <- Data$yearFirstDonation - Data$education_yearFirstGraduation
+
+ggplot(Data, aes(x = gap_Dipl_FirstDon)) +
+  geom_histogram(binwidth = 1, fill = "#FFC103",
+                 color = "#FFC103", alpha = 0.5) +
+  scale_x_continuous(limits = c(-10, 65),
+                     breaks = seq(-10, 65, by = 5)) +
+  xlab("\nNombre d'années entre le premier diplôme\net le premier don\n") +
+  ylab("\nNombre d'étudiants\n") +
+  clessnverse::theme_clean_light(base_size = 30) +
+  theme(axis.ticks.x = element_blank(),
+        axis.line.x = element_line(linewidth = 0),
+        text = element_text(lineheight = 0.3),
+        axis.title.x = element_text(size = 45),
+        axis.title.y = element_text(size = 45),
+        axis.text.x = element_text(size = 30),
+        axis.text.y = element_text(size = 30))
+
+Data %>% 
+  mutate(betw0_5 = ifelse(gap_Dipl_FirstDon >= 0 & gap_Dipl_FirstDon <= 5, 1, 0)) %>% 
+  group_by(betw0_5) %>% 
+  summarise(n = n()) %>% 
+  drop_na() %>% 
+  mutate(n_all = sum(n),
+         prop = round(n/n_all*100, 2))
+
+ggsave("_SharedFolder_fondation-ulaval/pres_2022-12-06/graphs/2.2.2.diplomaFirstDon.png",
+       width = 8, height = 5)
