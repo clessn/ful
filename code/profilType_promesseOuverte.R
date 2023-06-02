2900fdf6548fd58b95b8b087bdae8c4cdaa051e4
AlexBouillon <alexandrebouillon7@gmail.com>
2023-06-02 11:31:11 -0400

    modif graph dons planifiés

--- code/profilType_promesseOuverte.R
+++ code/profilType_promesseOuverte.R
@@ -19,6 +19,7 @@ Graph <- Data %>%
 ggplot(Graph, aes(y= prop, x= interaction(ses_female, ses_origin_qc))) + 
   geom_bar(stat = "identity", aes(fill = factor(ses_female), color = factor(ses_female)),
            alpha = 0.6, show.legend = FALSE) +
+  geom_text(label = paste(round(Graph$prop, 2),"%"), nudge_y = 0.03, size = 5.5) + 
   scale_x_discrete(labels = c(
     "Homme hors-Québec",
     "Femme hors-Québec",
@@ -30,10 +31,15 @@ ggplot(Graph, aes(y= prop, x= interaction(ses_female, ses_origin_qc))) +
                                  "0" = "#E30513")) +
   scale_y_continuous(limits = c(0, 1.5), breaks = c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5)) +
   labs(title = "Dons planifiés") +
-  ylab("Proportion (%)") +
+  ylab("Proportion (%) \n" ) +
   xlab(element_blank()) +
   theme_classic() +
-  theme(axis.ticks = element_blank())
+  theme(axis.ticks = element_blank(),
+        axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title.y = element_text(size = 15),
+        title = element_text(size = 20))
+
 
 ggsave("_SharedFolder_fondation-ulaval/graphs/profilsDonsPlanifiés.png",
        width = 12, height = 9)  
