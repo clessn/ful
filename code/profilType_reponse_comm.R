66e024b2a569dd086be4c1caa2474aad81b23f95
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2023-06-07 18:22:01 -0400

    ajustements de theme aux graphs profs et comm

--- code/profilType_reponse_comm.R
+++ code/profilType_reponse_comm.R
@@ -41,7 +41,7 @@ Graph <- dons_comm %>%
 ggplot(Graph, aes(y= prop, x= interaction(ses_female, ses_origin_qc))) + 
   geom_bar(stat = "identity", aes(fill = factor(ses_female), color = factor(ses_female)),
            alpha = 0.6, show.legend = FALSE) +
-  geom_text(label = paste(round(Graph$prop, 2),"%"), nudge_y = 0.03, size = 5.5) + 
+  geom_text(label = paste(round(Graph$prop, 2),"%"), nudge_y = 0.05, size = 5.5) + 
   scale_x_discrete(labels = c(
     "Homme hors-Québec",
     "Femme hors-Québec",
@@ -52,15 +52,17 @@ ggplot(Graph, aes(y= prop, x= interaction(ses_female, ses_origin_qc))) +
   scale_color_manual(values = c( "1" = "#FFC103",
                                  "0" = "#E30513")) +
   scale_y_continuous(limits = c(0, 2), breaks = c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2)) +
-  labs(title = "Réponse positive à la dernière communication") +
-  ylab("Proportion (%) \n") +
+  labs(title = "Réponse positive à la dernière communication",
+       subtitle = "Sont considérés comme ayant répondu positivement s'il y a un\ndon dans les 30 jours suivants la communication") +
+  ylab("Proportion de réponses\npositives (%)") +
   xlab(element_blank()) +
-  theme_classic() +
+  clessnverse::theme_clean_light() +
   theme(axis.ticks = element_blank(),
         axis.text.x = element_text(size = 15),
         axis.text.y = element_text(size = 15),
-        axis.title.y = element_text(size = 15),
+        axis.title.y = element_text(size = 20,
+                                    hjust = 0.5),
         title = element_text(size = 20))
 
-ggsave("_SharedFolder_fondation-ulaval/graphs/profilsDonsPlanifiés.png",
+ggsave("_SharedFolder_fondation-ulaval/graphs/profilsReponseComm.png",
        width = 12, height = 9)
