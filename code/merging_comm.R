adb631281736ba9d851e52a20f9b94c330bb6c83
hubcad25 <hubertcadieux@gmail.com>
2023-09-13 16:45:43 -0400

    merge comm data

---
+++ code/merging_comm.R
@@ -0,0 +1,18 @@
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+
+# Data --------------------------------------------------------------------
+files <- list.files("_SharedFolder_fondation-ulaval/Data/DataComm")
+
+for (i in 1:length(files)){
+  d <- read.csv(file = paste0("_SharedFolder_fondation-ulaval/Data/DataComm/", files[i]),
+                fileEncoding = "ISO-8859-1")
+  if (i == 1){
+    df <- d
+  } else {
+    df <- rbind(df, d)
+  }
+  print(i)
+}
+
+saveRDS(d, "_SharedFolder_fondation-ulaval/Data/pipeline/lake/datacomm.rds")
